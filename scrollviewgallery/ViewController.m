d207dd2df799d343ecf6c55deb9f1137c938cc7a
will <will@willlam.net>
2016-03-21 18:35:09 -0400

    added scrollview and images

--- scrollviewgallery/ViewController.m
+++ scrollviewgallery/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+
+
 
 @end
 
@@ -17,11 +22,41 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	
+	UIImageView *imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-in-Field.jpg"]];
+//	imageView1.translatesAutoresizingMaskIntoConstraints = NO;
+	
+	UIImageView *imageView2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-night.jpg"]];
+//	imageView2.translatesAutoresizingMaskIntoConstraints = NO;
+	
+	UIImageView *imageView3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse.jpg"]];
+//	imageView3.translatesAutoresizingMaskIntoConstraints = NO;
+	
+	[self.scrollView addSubview:imageView1];
+	[self.scrollView addSubview:imageView2];
+	[self.scrollView addSubview:imageView3];
+	
+	
+	
+	self.scrollView.contentSize = CGSizeMake(imageView1.frame.size.width * 3, self.view.bounds.size.height);
+	
+	self.scrollView.pagingEnabled = YES;
+	
+	
+	
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
+//- (void) prepareScrollView {
+//	[self.scrollView setContentSize:CGSize(self.
+//}
+
+	
+
 
 @end
